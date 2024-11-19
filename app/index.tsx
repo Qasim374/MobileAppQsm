798cb33e98788f7e9a51b560e5a30e6dc326b1a8
DESKTOP-1E3R0VQ\Qasim <qasim.bcs12@gmail.com>
2024-11-19 18:09:47 +0500

    new file

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "black",
+      }}
+    >
+      <Text style={{ fontSize: 24, color: "white" }}>Hello World!</Text>
+    </View>
+  );
+}
