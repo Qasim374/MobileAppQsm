b6215338889f5e5e6b6198e7bd2440d78667eaf7
DESKTOP-1E3R0VQ\Qasim <qasim.bcs12@gmail.com>
2024-11-20 23:07:10 +0500

    update

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,95 @@
+import { Image, Text, View } from "react-native";
+import React from "react";
+import { Tabs, Redirect } from "expo-router";
+import { icons } from "../../constants";
+
+const TabIcon = ({ icon, name, color, focused }) => {
+  return (
+    <View className="flex items-center justify-center gap-0">
+      <Image
+        source={icon}
+        resizeMode="cover"
+        tintColor={color}
+        style={{ width: 20, height: 20 }}
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
+
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs screenOptions={{ tabBarShowLabel: false }}>
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="bookmark"
+          options={{
+            title: "Bookmark",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.bookmark}
+                color={color}
+                name="Bookmark"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.plus}
+                color={color}
+                name="Create"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.profile}
+                color={color}
+                name="Profile"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  );
+};
+
+export default TabsLayout;
--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,8 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="font-bold text-6xl">Aora!</Text>
-      <Link href="/profile" className="text-blue-700">
+      <Text className="font-pblack text-2xl">Aora!</Text>
+      <Link href="/home" className="text-blue-700">
         Go to profile
       </Link>
     </View>
