b6c4f584deeefd7db439a500e9d826c39d52b424
DESKTOP-1E3R0VQ\Qasim <qasim.bcs12@gmail.com>
2024-11-20 17:05:09 +0500

    update

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="font-bold text-3xl">Aora!</Text>
-      <Text className="text-3xl font-bold text-red-800">Khan</Text>
+      <Text className="font-bold text-6xl">Aora!</Text>
+      <Link href="/profile" className="text-blue-700">
+        Go to profile
+      </Link>
     </View>
   );
 }
