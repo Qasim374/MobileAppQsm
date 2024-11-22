bd3ba36844143d24afc84e6198aa66ede3c08021
DESKTOP-1E3R0VQ\Qasim <qasim.bcs12@gmail.com>
2024-11-22 21:50:33 +0500

    changes

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "@/components/FormField";
 
 const signIn = () => {
   return (
-    <View>
-      <Text>signIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full min-h-[85vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            // className="w-[115px] h-[15px]"
+            style={{ width: 115, height: 35 }}
+          />
+          <Text className="font-semibold text-2xl text-white mt-7 font-psemibold">
+            Login to Aora
+          </Text>
+          <FormField />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
---
+++ components/FormField.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+const FormField = () => {
+  return (
+    <View className="space-y-2">
+      <Text className="text-gray-100 font-pmedium text-base">FormField</Text>
+    </View>
+  );
+};
+
+export default FormField;
